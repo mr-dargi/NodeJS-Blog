599180908f2796a6977e0737bf456d9b8d366e45
mr-dargi <dargimohammadreza@gmail.com>
2023-02-20 12:26:17 +0330

    Path html page as a response to the page

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     // set header content type
     res.setHeader("Content-Type", "text/html");
-
-    res.write("hello world!");
-    res.end();
+    fs.readFile("./views/main.html", (err, data) => {
+        if(err) {
+                console.log(err);
+                res.end;
+        }else {
+            // res.write(data);
+            res.end(data);
+        }
+    });
 })
 
 server.listen(3000, "localhost", () => {
