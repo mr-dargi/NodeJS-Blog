785e18dbf7129715338bb2e466d341124dfe05f0
mr-dargi <dargimohammadreza@gmail.com>
2023-02-21 15:07:40 +0330

    Passing data into our views

--- app.js
+++ app.js
@@ -11,20 +11,25 @@ app.set("view engine", "ejs");
 app.listen(3000);
 
 app.get("/", (req, res) => {
-   res.render("main");
+    const blogs = [
+        {title: "Some time see you past out side my door", snippet: "Lorem ipsum dolor, sit amet consectetur adipisicing elit."},
+        {title: "Mohammad reza Dargi", snippet: "Lorem ipsum dolor, sit amet consectetur adipisicing elit."},
+        {title: "How to defeat browser", snippet: "Lorem ipsum dolor, sit amet consectetur adipisicing elit."}
+    ]
+   res.render("main", { title: "HOME", blogs });
 });
 
 
 app.get("/about", (req, res) => {
-    res.render("about")
+    res.render("about", { title: "ABOUT" });
  });
 
 
 app.get("/blogs/create", (req, res) => {
-    res.render("create");
+    res.render("create", { title: "CREATE NEW BLOG" });
 });
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).render("404");
+    res.status(404).render("404", { title: "404" });
 })
