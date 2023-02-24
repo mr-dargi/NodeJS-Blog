ee4ef3b7740ab3c8bad1cae065b595eacec302a4
mr-dargi <dargimohammadreza@gmail.com>
2023-02-24 14:49:45 +0330

    Make NoSql data base on mongodb atlas using mongoose package to handel working to data base and in the end getting and save data in database

--- app.js
+++ app.js
@@ -1,26 +1,52 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
+mongoose.set("strictQuery", false);
 
 // express app
 const app = express();
+
+// conect to mongodb 
+const username = encodeURIComponent("");
+const password = encodeURIComponent("");
+const dbURI = `mongodb+srv://${username}:${password}@cluster0.9pimv74.mongodb.net/nodeBlog?retryWrites=true&w=majority`;
+
+
+
+mongoose.connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+
 
 // register view engine
 app.set("view engine", "ejs");
 
 
-// listen for request
-app.listen(3000);
-
 // middleware and & static files
 app.use(express.static("public"));
-
-
-// app.use((req, res, next) => {
-    
-//     next();
-// })
-
 app.use(morgan("dev"));
+
+
+// mongoose and mongo sendbox routes
+app.get('/add-blog', (req, res) => {
+const blog = new Blog({
+        title: 'new blog',
+        snippet: 'about my new blog',
+        body: 'more about my new blog'
+    })
+
+    blog.save()
+        .then(result => {
+        res.send(result);
+        })
+        .catch(err => {
+        console.log(err);
+        });
+});
+
 
 app.get("/", (req, res) => {
     const blogs = [
